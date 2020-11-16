47bdf27ac0b45b2f2c0949ddc4aacede29743cdc
Gustavo Luiz Stahnke <gustavo.stahnke@gmail.com>
2020-11-16 18:57:48 -0300

    av5

--- pg/back-end/classe.py
+++ pg/back-end/classe.py
@@ -9,7 +9,7 @@ class Moto(db.Model):
     peso = db.Column(db.String(254))
     cilindradas = db.Column(db.String(254))
 
-    def __str__(self):
+    def _str_(self):
         return str(self.id)+", " + self.marca + ", " +\
             self.modelo + ", " + self.velocidade + ", " +\
             self.peso + ", " + self.cilindradas
@@ -22,4 +22,50 @@ class Moto(db.Model):
             "velocidade": self.velocidade,
             "peso": self.peso,
             "cilindradas": self.cilindradas
+        })
+
+class Revendedora(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True) 
+    localizacao = db.Column(db.String(254)) 
+    montadora = db.Column(db.String(254))
+    telefone = db.Column(db.Integer)
+    moto_id = db.Column(db.Integer, db.ForeignKey(Moto.id))
+    moto = db.relationship("Moto")
+
+    def _str_(self): 
+        return self.localizacao + ", " + self.montadora +\
+            str(self.telefone) + str(self.moto)
+
+    def json(self):
+        return ({
+            "id": self.id,
+            "localizacao": self.localizacao,
+            "montadora": self.montadora,
+            "telefone": self.telefone,
+            "moto_id": self.moto_id,
+            "moto": self.moto.json()
+        }) 
+
+class Comprador(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    telefone = db.Column(db.Integer)
+    nome = db.Column(db.String(254))
+    cpf = db.Column(db.Integer)
+    moto_id = db.Column(db.Integer, db.ForeignKey(Moto.id))
+    moto = db.relationship("Moto")
+
+    def _str_(self):
+        return str(self.telefone) + ", " + self.nome +\
+            str(self.cpf)+ str(self.moto)
+
+    def json(self):
+        return ({
+            "id":self.id,
+            "telefone":self.telefone,
+            "nome": self.nome,
+            "cpf": self.cpf,
+            "moto_id": self.moto_id,
+            "moto": self.moto.json()
         })
--- pg/back-end/teste.py
+++ pg/back-end/teste.py
@@ -15,11 +15,22 @@ if __name__ == "__main__":
     m3 = Moto(marca = "Honda", modelo = "Iron 883", 
     velocidade = "160", peso = "256", cilindradas = "883")
 
+    r1 = Revendedora(localizacao = "Blumenau", telefone ="3333-3333", montadora = "Yamaha", moto = m1)
+
+    c1 = Comprador(telefone = "99999-9999", nome = "Arnaldo", cpf = "012.012.012-12", moto = m1)
+
     db.session.add(m1)
     db.session.add(m2)
     db.session.add(m3)
+
+    db.session.add(r1)
+
+    db.session.add(c1)
+    
     db.session.commit()
     
     print(m1.json())
     print(m2.json())
     print(m3.json())
+    print(r1.json())
+    print(c1.json())
